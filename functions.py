cbe5a0ba3ded6fc822a975abeec959c81e8a7f13
Sterrrrest <150155969+Sterrrrest@users.noreply.github.com>
2024-05-15 13:53:25 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,25 @@
+import telebot
+import os
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
+chat_id = env.str('CLIENT_ID')
+token_tg = env.str('TOKEN_TG')
+bot = telebot.TeleBot(token=token_tg)
+
+
+def send_post(file_path, comment, response_img):
+
+    with open(file_path, 'wb') as file:
+        file.write(response_img.content)
+
+    with open(file_path, 'rb') as file:
+        bot.send_document(chat_id=chat_id, document=file)
+
+    bot.send_message(chat_id=chat_id, text=comment)
+
+    os.remove(file_path)
+
---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+import requests
+
+from pathlib import Path
+from functions import send_post
+
+
+if __name__ == '__main__':
+
+    dir = 'pictures'
+    Path(dir).mkdir(parents=True, exist_ok=True)
+
+    last_comics_url = 'https://xkcd.com/info.0.json'
+    response_last_comics = requests.get(last_comics_url)
+    response_last_comics.raise_for_status()
+    last_comics = response_last_comics.json()['num']
+
+    random_comics = random.choice(range(1, last_comics))
+
+    response_random_url = f'https://xkcd.com/{random_comics}/info.0.json'
+    response_random_comics = requests.get(response_random_url)
+
+    response_img = requests.get(response_random_comics.json()['img'])
+    response_img.raise_for_status()
+
+    filename = response_random_comics.json()['img'].split(sep='/')[-1]
+    file_path = f'{dir}/{filename}'
+    comment = response_random_comics.json()['alt']
+
+    send_post(file_path, comment, response_img)
+
